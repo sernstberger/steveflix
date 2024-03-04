57a6de22cd6a266d6cded236b87dc0dd7b57c4ee
Steve Ernstberger <steve32285@gmail.com>
2024-03-04 13:01:43 -0500

    added video card component

--- apps/steveflix/src/app/app.tsx
+++ apps/steveflix/src/app/app.tsx
@@ -1,9 +1,46 @@
-import { Card } from '@mui/joy';
+import VideoCard from '../components/VideoCard';
+import Card from '@mui/joy/Card';
+import CardCover from '@mui/joy/CardCover';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+
+export function Foo() {
+  return (
+    <Card sx={{ minWidth: 300, flexGrow: 1 }}>
+      <CardCover>
+        <video
+          autoPlay
+          loop
+          muted
+          poster="https://assets.codepen.io/6093409/river.jpg"
+        >
+          <source
+            src="https://assets.codepen.io/6093409/river.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </CardCover>
+      <CardContent>
+        <Typography
+          level="body-lg"
+          fontWeight="lg"
+          textColor="#fff"
+          mt={{ xs: 12, sm: 18 }}
+        >
+          Video
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
 
 export function App() {
   return (
     <div>
-      <Card>asjdlf</Card>
+      <Foo />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
     </div>
   );
 }
