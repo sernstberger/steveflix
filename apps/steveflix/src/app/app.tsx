6f28e62689213d5a1618a13f9f408877a2bee96a
Steve Ernstberger <steve32285@gmail.com>
2024-03-05 07:25:01 -0500

    added react router

--- apps/steveflix/src/app/app.tsx
+++ apps/steveflix/src/app/app.tsx
@@ -1,17 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Billboard } from '../components/Billboard';
 import VideoCardRow from '../components/VideoCardRow';
 import VideoPlayer from '../components/VideoPlayer';
 
 export function App() {
   return (
-    <div>
-      <VideoPlayer />
-      <Billboard />
-
-      <VideoCardRow />
-      <VideoCardRow />
-      <VideoCardRow />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Billboard />
+              <VideoCardRow />
+              <VideoCardRow />
+              <VideoCardRow />
+            </div>
+          }
+        />
+        <Route path="/preview/:id" element={<div>Preview Modal</div>} />
+        <Route path="/play/:id" element={<VideoPlayer />} />
+      </Routes>
+    </Router>
   );
 }
 
