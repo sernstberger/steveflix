c161a1fa56576332dbadec500dfd4051fbd5b501
Steve Ernstberger <steve32285@gmail.com>
2024-03-05 10:57:31 -0500

    added movie info

--- apps/steveflix/src/components/VideoPreview/index.tsx
+++ apps/steveflix/src/components/VideoPreview/index.tsx
@@ -1,7 +1,8 @@
 import Typography from '@mui/joy/Typography';
 
 import Sheet from '@mui/joy/Sheet';
-import { Button } from '@mui/joy';
+import { Button, Stack } from '@mui/joy';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { Link, useParams } from 'react-router-dom';
 import { useGetMovieQuery } from '../../redux/movieApi';
 
@@ -11,42 +12,50 @@ export default function VideoPreview() {
 
   if (isLoading) return 'Loading...';
 
-  console.log(movie);
+  const director = movie.credits.crew.find(
+    (person: any) => person.job === 'Director'
+  );
+  console.log(director);
 
   return (
     <Sheet
-      variant="outlined"
+      variant="plain"
       sx={{
+        backgroundImage: `linear-gradient(0deg, #000000, transparent), url("https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.backdrop_path}")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        // backgroundImage: `linear-gradient(transparent, rgba(0, 0, 0, 0.7))`,
+        height: '100vh',
         p: 3,
       }}
     >
-      <Typography
-        component="p"
-        id="modal-title"
-        level="h4"
-        textColor="inherit"
-        fontWeight="lg"
-        // mb={1}
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="flex-end"
+        sx={{ height: '100%' }}
       >
-        {movie.title}
-      </Typography>
+        {/* https://media.themoviedb.org/t/p/w300_and_h450_bestv2/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg */}
+        <img
+          src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+          alt="alsdkjf"
+        />
 
-      {/* url('https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg') */}
-      <img
-        src={`https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.backdrop_path}`}
-        alt="alsdkjf"
-      />
+        <div>
+          <Typography level="h1">{movie.title}</Typography>
+          <Typography>{movie.overview}</Typography>
+          <Button
+            component={Link}
+            to={`/${movie.id}/playing`}
+            startDecorator={<PlayArrowIcon />}
+          >
+            Play
+          </Button>
 
-      {/* https://media.themoviedb.org/t/p/w300_and_h450_bestv2/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg */}
-      <img
-        src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
-        alt="alsdkjf"
-      />
-
-      <Typography>{movie.overview}</Typography>
-      <Button component={Link} to={`/${movie.id}/playing`}>
-        Play
-      </Button>
+          <Typography>Cast: {movie.credits.cast[0].name}</Typography>
+          <Typography>Director: {director.name}</Typography>
+        </div>
+      </Stack>
     </Sheet>
   );
 }
