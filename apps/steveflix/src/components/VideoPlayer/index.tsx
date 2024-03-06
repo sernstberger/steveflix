fcf8789673817cb80876202ecd1252b12baa4d2e
Steve Ernstberger <steve32285@gmail.com>
2024-03-05 20:06:23 -0500

    improving player

--- apps/steveflix/src/components/VideoPlayer/index.tsx
+++ apps/steveflix/src/components/VideoPlayer/index.tsx
@@ -1,4 +1,6 @@
 import React, { useRef } from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import { Box, Button, IconButton, Slider, Stack, Typography } from '@mui/joy';
 
 export default function VideoPlayer() {
   const videoRef = useRef<any>(null);
@@ -11,25 +13,48 @@ export default function VideoPlayer() {
     videoRef.current?.pause();
   };
 
-  const stopVideo = () => {
-    videoRef.current?.pause();
-    videoRef.current.currentTime = 0;
-  };
-
   return (
-    <div>
-      <video ref={videoRef} width="320" height="240">
-        <source
-          src="https://assets.codepen.io/6093409/river.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
-      <div>
-        <button onClick={playVideo}>Play</button>
-        <button onClick={pauseVideo}>Pause</button>
-        <button onClick={stopVideo}>Stop</button>
-      </div>
-    </div>
+    <Box
+      position="relative"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ height: '100vh', width: '100vw' }}
+    >
+      <AspectRatio sx={{ width: '100vw' }}>
+        <video ref={videoRef} width="320" height="240">
+          <source
+            src="https://assets.codepen.io/6093409/river.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </AspectRatio>
+
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        padding={5}
+        height="100vh"
+        width="100vw"
+      >
+        <Stack justifyContent="space-between" height="100%">
+          <div>
+            <Typography>Rated PG-13</Typography>
+            <Typography>Drinking</Typography>
+          </div>
+
+          <IconButton sx={{ alignSelf: 'center' }} onClick={playVideo}>
+            Play
+          </IconButton>
+          {/* <button onClick={pauseVideo}>Pause</button> */}
+          <div>
+            <Typography level="h1">MovieName</Typography>
+            <Slider value={40} />
+          </div>
+        </Stack>
+      </Box>
+    </Box>
   );
 }
